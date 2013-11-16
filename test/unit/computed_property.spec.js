c29e52fd3946b18b3b9f30da31c48aecb9ef4f21
Ryan Florence <rpflorence@gmail.com>
2013-11-16 14:02:23 -0700

    computed_property passing

--- test/unit/computed_property.spec.js
+++ test/unit/computed_property.spec.js
@@ -1,13 +1,19 @@
 var computed = require('../../lib/computed_property');
 
 describe('computed_property', function() {
-  it('returns non-function properties', function() {
-    computed('foo').should.eql('foo');
+  it('returns non-function properties', function(done) {
+    computed('foo', function(val) {
+      val.should.eql('foo');
+      done();
+    });
   });
 
-  it("returns a function's result", function() {
-    var foo = function() { return 'foo'; };
-    computed(foo).should.eql('foo');
+  it("returns a function's result", function(done) {
+    var foo = function(callback) { callback('foo'); };
+    computed(foo, function(val) {
+      val.should.eql('foo');
+      done();
+    });
   });
 });
 
