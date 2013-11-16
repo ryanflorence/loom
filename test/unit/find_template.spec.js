88e60c944c09123e04aa0c2a8a64c3dadfa62a19
Ryan Florence <rpflorence@gmail.com>
2013-11-16 14:56:06 -0700

    find_template passing

--- test/unit/find_template.spec.js
+++ test/unit/find_template.spec.js
@@ -2,15 +2,21 @@ var findTemplate = require('../../lib/find_template');
 var fixture = require('../support/fixture');
 
 describe('find_template', function() {
-  it('finds a template', function() {
+  it('finds a template', function(done) {
     var env = { paths: ['test/fixtures', 'fake/stuff'] };
     var expected = 'test/fixtures/templates/app/model.js.hbs';
-    findTemplate('app/model.js.hbs', env).should.equal(expected);
+    findTemplate('app/model.js.hbs', env, function(template) {
+      template.should.equal(expected);
+      done();
+    });
   });
 
-  it('returns false if no template is found', function() {
+  it('returns false if no template is found', function(done) {
     var env = { paths: ['test/fixtures', 'fake/stuff'] };
-    findTemplate('fake.js.hbs', env).should.be.false;
+    findTemplate('fake.js.hbs', env, function(template) {
+      template.should.be.false;
+      done();
+    });
   });
 });
 
